d1a5eb03ccc3d853085f542b847108995e638fff
MirRoN <miron.mvi@gmail.com>
2014-07-03 12:20:56 +0300

    Fixed bug in  GameFieldCellsFlyweightFactory - all cells were with value 1

--- Baloons-Pop-4/Factories/GameFieldCellsFlyweightFactory.cs
+++ Baloons-Pop-4/Factories/GameFieldCellsFlyweightFactory.cs
@@ -12,9 +12,9 @@
         static GameFieldCellsFlyweightFactory()
         {
             GameFieldCellsFlyweightFactory.flywieghtCells.Add(1, new GameFieldCell(1, ConsoleColor.White, ConsoleColor.Red));
-            GameFieldCellsFlyweightFactory.flywieghtCells.Add(2, new GameFieldCell(1, ConsoleColor.Black, ConsoleColor.Green));
-            GameFieldCellsFlyweightFactory.flywieghtCells.Add(3, new GameFieldCell(1, ConsoleColor.White, ConsoleColor.Blue));
-            GameFieldCellsFlyweightFactory.flywieghtCells.Add(4, new GameFieldCell(1, ConsoleColor.Black, ConsoleColor.Yellow));
+            GameFieldCellsFlyweightFactory.flywieghtCells.Add(2, new GameFieldCell(2, ConsoleColor.Black, ConsoleColor.Green));
+            GameFieldCellsFlyweightFactory.flywieghtCells.Add(3, new GameFieldCell(3, ConsoleColor.White, ConsoleColor.Blue));
+            GameFieldCellsFlyweightFactory.flywieghtCells.Add(4, new GameFieldCell(4, ConsoleColor.Black, ConsoleColor.Yellow));
         }
 
         public static GameFieldCell GetCellByValue(int value)
