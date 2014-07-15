d37179dcd4a930841c78bfa88e61bf048930f01e
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-07-16 01:40:33 +0300

    Unit testing in progress. FactoriesNS tests added

---
+++ BaloonsPopsGame.Tests/ClassicalGameFieldFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿namespace BaloonsPopsGame.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BaloonsPopsGame;
+
+    [TestClass]
+    public class ClassicalGameFieldFactoryTest
+    {
+        [TestMethod]
+        public void GameFieldFactoryCreatesTheSameInstance()
+        {
+            var gameFieldFactory = new Factories.ClassicalGameFieldFactory();
+            var firstInstance = gameFieldFactory.Create();
+            var secondInstance = gameFieldFactory.Create();
+            Assert.AreSame(firstInstance, secondInstance);
+        }
+    }
+}
