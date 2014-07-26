a6d9c86cdf65729f0b20b995873560416b688200
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-07-26 16:55:20 +0300

    highscores tests added

---
+++ BaloonsPopsGame.Tests/ConsoleInput.cs
@@ -0,0 +1,24 @@
+﻿namespace BaloonsPopsGame.Tests
+{
+    using System;
+    using System.IO;
+
+    public class ConsoleInput : IDisposable
+    {
+        private StringReader stringReader;
+        private TextReader originalOutput;
+
+        public ConsoleInput(string input)
+        {
+            this.stringReader = new StringReader(input);
+            this.originalOutput = Console.In;
+            Console.SetIn(this.stringReader);
+        }
+
+        public void Dispose()
+        {
+            Console.SetIn(this.originalOutput);
+            this.stringReader.Dispose();
+        }
+    }
+}
---
+++ BaloonsPopsGame.Tests/Utilities/HighScores.cs
@@ -0,0 +1,47 @@
+﻿namespace BaloonsPopsGame.Tests.Utilities
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using BaloonsPopsGame.Utilities;
+
+    [TestClass]
+    public class HighScoresTest
+    {
+        [TestMethod]
+        public void HighScoresSavesToFile()
+        {
+            var input = new string[0, 0];
+            var path = "testInput.txt";
+            HighScores.Save(input, path);
+            string result;
+
+            using (var reader = new StreamReader(path))
+            {
+                result = reader.ReadToEnd();
+            }
+            var expected = "0" + Environment.NewLine + "0" + Environment.NewLine;
+
+            Assert.AreEqual(expected, result);
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void HighScoresSavesAndLoadsTheSame()
+        {
+            var input = new string[0, 0];
+            var path = "testInput.txt";
+            HighScores.Save(input, path);
+
+            var result = HighScores.Load(path);
+
+
+            foreach (var item in result)
+            {
+                Assert.AreEqual(input, result);
+            }
+
+            File.Delete(path);
+        }
+    }
+}
